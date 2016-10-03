ed8d3f517dcee8b066b7985b0bd0e1599241d384
Evgeny Boykov <artscan@list.ru>
2016-10-03 14:46:32 +1000

    add working config for ldap 2.3 centos 5

---
+++ Makefile
@@ -0,0 +1,16 @@
+build-server:
+	cd ldap-server && docker build -t cc-ldap-dev5 .
+	docker run --name cc-ldap-data5 -v /data busybox true || true
+	docker run --name cc-ldap-centos5 --volumes-from cc-ldap-data5 cc-ldap-dev5
+
+test:
+	docker start cc-ldap-centos5
+	$(eval IPcentos5 := $(shell docker inspect -f {{.NetworkSettings.IPAddress}} cc-ldap-centos5))
+	sleep 1
+	cd ldap-server && ldapadd -h $(IPcentos5) -x -D 'cn=Manager,dc=tuleap,dc=local' -w manager -f bob.ldif || true
+	cd ldap-server && ldapadd -h $(IPcentos5) -x -D 'cn=Manager,dc=tuleap,dc=local' -w manager -f admin.ldif || true
+	ldapsearch -x -h $(IPcentos5) -LLL -D 'cn=Manager,cn=config' -b 'dc=tuleap,dc=local' '*' -w root
+
+clear:
+	docker rm -f -v cc-ldap-centos5
+	docker rm cc-ldap-data5
---
+++ ldap-server/run.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# for testing purpose
+LDAP_ROOT_PASSWORD=root
+LDAP_MANAGER_PASSWORD=manager
+
+set -e
+
+if [ ! -f /data/lib/ldap/DB_CONFIG ]; then
+    if [ -z "$LDAP_ROOT_PASSWORD" -o -z "$LDAP_MANAGER_PASSWORD" ]; then
+	echo "Need LDAP_ROOT_PASSWORD and LDAP_MANAGER_PASSWORD"
+	exit
+    fi
+
+    cp /etc/openldap/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
+    chown ldap. /var/lib/ldap/DB_CONFIG
+
+    rm -f /etc/openldap/slapd.conf
+    cp /root/slapd.conf /etc/openldap/slapd.conf
+    chown ldap. /etc/openldap/slapd.conf
+
+    service ldap start
+    sleep 3
+
+    killall slapd
+
+    oldpath=`pwd`
+    cd /etc/openldap
+    mkdir slapd.d
+    slaptest -f slapd.conf -F slapd.d
+    chown -R ldap:ldap slapd.d
+    chmod -R 0750 slapd.d
+    mv slapd.conf slapd.conf.bak
+    cd $oldpath
+
+    service ldap start
+    sleep 3
+
+    ROOT_PWD=$(slappasswd -s $LDAP_ROOT_PASSWORD)
+    # Use bash variable subsitution to escape special chars http://stackoverflow.com/a/14339705
+    sed -i "s+%LDAP_ROOT_PASSWORD%+${ROOT_PWD//+/\\+}+" /root/manager.ldif
+    ldapmodify -v -D cn=Manager,cn=config -f /root/manager.ldif -x -w 1
+
+    MANAGER_PWD=$(slappasswd -s $LDAP_MANAGER_PASSWORD)
+    sed -i "s+%LDAP_MANAGER_PASSWORD%+${MANAGER_PWD//+/\\+}+" /root/domain.ldif
+    ldapmodify  -v -D cn=Manager,cn=config -f /root/domain.ldif -x -w $LDAP_ROOT_PASSWORD
+
+    ldapadd -x -D cn=Manager,dc=tuleap,dc=local -w $LDAP_MANAGER_PASSWORD -f /root/base.ldif
+
+    # kill -INT `cat /var/run/openldap/slapd.pid`
+    # service ldap stop
+    killall slapd
+    sleep 3
+
+    mkdir /data/lib /data/etc
+    cp -ar /var/lib/ldap /data/lib
+    cp -ar /etc/openldap /data/etc
+fi
+
+rm -rf /var/lib/ldap && ln -s /data/lib/ldap /var/lib/ldap
+rm -rf /etc/openldap && ln -s /data/etc/openldap /etc/openldap
+
+exec /usr/sbin/slapd -h "ldap:/// ldaps:/// ldapi:///" -u ldap -d $DEBUG_LEVEL

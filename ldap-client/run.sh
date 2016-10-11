472ea3624bc0ccf7f3643738977275e376bd996e
Evgeny Boykov <artscan@list.ru>
2016-10-11 10:32:33 +1000

    fix client/run*: add straight diff for /etc

--- ldap-client/run.sh
+++ ldap-client/run.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 
-cp /etc/group /gen/etc5.original/group
-cp /etc/group- /gen/etc5.original/group-
-cp /etc/ldap.conf /gen/etc5.original/ldap.conf
-cp /etc/nsswitch.conf /gen/etc5.original/nsswitch.conf
-cp /etc/openldap/ldap.conf /gen/etc5.original/openldap-ldap.conf
-cp /etc/pam.d/system-auth /gen/etc5.original/system-auth
-cp /etc/passwd /gen/etc5.original/passwd
-cp /etc/passwd- /gen/etc5.original/passwd-
-cp /etc/sysconfig/authconfig /gen/etc5.original/authconfig
+cp -R /etc /gen/etc5/etc
 
 authconfig --enableshadow --enablemkhomedir --enableldap --enableldapauth --ldapserver=$LDAP_SERVER --ldapbasedn=$LDAP_BASEDN --update
 
-cp /etc/group /gen/etc5/group
-cp /etc/group- /gen/etc5/group-
-cp /etc/gshadow /gen/etc5/gshadow
-cp /etc/ldap.conf /gen/etc5/ldap.conf
-cp /etc/nsswitch.conf /gen/etc5/nsswitch.conf
-cp /etc/openldap/ldap.conf /gen/etc5/openldap-ldap.conf
-cp /etc/pam.d/system-auth /gen/etc5/system-auth
-cp /etc/passwd /gen/etc5/passwd
-cp /etc/passwd- /gen/etc5/passwd-
-cp /etc/shadow /gen/etc5/shadow
-cp /etc/sysconfig/authconfig /gen/etc5/authconfig
+cp -R /etc /gen/etc5/etcnew
 
-diff -u /gen/etc5.original/ldap.conf /gen/etc5/ldap.conf > /gen/client.diff || true
-diff -u /gen/etc5.original/nsswitch.conf /gen/etc5/nsswitch.conf >> /gen/client.diff || true
-diff -u /gen/etc5.original/openldap-ldap.conf /gen/etc5/openldap-ldap.conf >> /gen/client.diff || true
-diff -u /gen/etc5.original/system-auth /gen/etc5/system-auth >> /gen/client.diff || true
-diff -u /gen/etc5.original/authconfig /gen/etc5/authconfig >> /gen/client.diff || true
+diff -u -r -x passwd -x group -x shadow /gen/etc5/etc /gen/etc5/etcnew > /gen/client.diff || true
+
+rm -rf /gen/etc5/etc
+rm -rf /gen/etc5/etcnew
 
 getent passwd username
 
--- ldap-client/run6.sh
+++ ldap-client/run6.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+cp -R /etc /gen/etc6/etc
+
 authconfig --enablemkhomedir --enableldap --enableldapauth --ldapserver=$LDAP_SERVER --ldapbasedn=$LDAP_BASEDN --update
+
+cp -R /etc /gen/etc6/etcnew
+
+diff -u -r /gen/etc6/etc /gen/etc6/etcnew > /gen/client6.diff || true
+
+rm -rf /gen/etc6/etc
+rm -rf /gen/etc6/etcnew
 
 getent passwd username
 

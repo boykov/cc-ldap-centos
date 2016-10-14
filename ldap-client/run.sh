ebbcab0e993034a199a368b794a72ba4f089241b
Evgeny Boykov <artscan@list.ru>
2016-10-14 13:36:15 +1000

    fix client*.diff, add client-files.diff, add text, headings

--- ldap-client/run.sh
+++ ldap-client/run.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#authentification][client5-run-prefix]]
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#client-packages][client5-run-prefix]]
 
 cp -R /etc /gen/etc5/etc
 
 # client5-run-prefix ends here
-# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#authentification][client5-run-setup]]
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#client-packages][client5-run-setup]]
 authconfig --enableshadow --enablemkhomedir --enableldap --enableldapauth --ldapserver=$LDAP_SERVER --ldapbasedn=$LDAP_BASEDN --update
 # client5-run-setup ends here
-# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#authentification][client-run-sudoers]]
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#client-packages][client-run-sudoers]]
 
 echo sudoers_base dc=mercury,dc=febras,dc=net >> /etc/ldap.conf
 echo sudoers_debug 0 >> /etc/ldap.conf
 # client-run-sudoers ends here
-# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#authentification][client5-run-postfix]]
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#client-packages][client5-run-postfix]]
 
 cp -R /etc /gen/etc5/etcnew
 
-diff -u -r -x passwd -x group -x shadow /gen/etc5/etc /gen/etc5/etcnew > /gen/client.diff || true
+diff -u -r --unidirectional-new-file /gen/etc5/etc /gen/etc5/etcnew > /gen/client.diff || true
+diff -q -r /gen/etc5/etc /gen/etc5/etcnew > /gen/client-files.diff || true
 
 rm -rf /gen/etc5/etc
 rm -rf /gen/etc5/etcnew
@@ -27,4 +28,4 @@ getent passwd username
 
 /usr/share/openldap/migration/migrate_group.pl /etc/group > /gen/group.ldif
 # client5-run-postfix ends here
-# Настройка\ аутентификации\ пользователей\ при\ помощи\ LDAP:1 ends here
+# Используемые\ пакеты:1 ends here
--- ldap-client/run6.sh
+++ ldap-client/run6.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#authentification][client6-run-prefix]]
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#client-packages][client6-run-prefix]]
 
 cp -R /etc /gen/etc6/etc
 
 # client6-run-prefix ends here
-# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#authentification][client6-run-setup]]
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#client-packages][client6-run-setup]]
 authconfig --enablemkhomedir --enableldap --enableldapauth --ldapserver=$LDAP_SERVER --ldapbasedn=$LDAP_BASEDN --update
 # client6-run-setup ends here
-# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#authentification][client-run-sudoers]]
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#client-packages][client-run-sudoers]]
 
 echo sudoers_base dc=mercury,dc=febras,dc=net >> /etc/ldap.conf
 echo sudoers_debug 0 >> /etc/ldap.conf
 # client-run-sudoers ends here
-# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#authentification][client6-run-postfix]]
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#client-packages][client6-run-postfix]]
 
 cp -R /etc /gen/etc6/etcnew
 
-diff -u -r /gen/etc6/etc /gen/etc6/etcnew > /gen/client6.diff || true
+diff -u -r --unidirectional-new-file /gen/etc6/etc /gen/etc6/etcnew > /gen/client6.diff || true
+diff -q -r /gen/etc6/etc /gen/etc6/etcnew > /gen/client6-files.diff || true
 
 rm -rf /gen/etc6/etc
 rm -rf /gen/etc6/etcnew
@@ -27,4 +28,4 @@ getent passwd username
 # /usr/share/migrationtools/migrate_passwd.pl /etc/passwd > /gen/passwd6.ldif
 # /usr/share/migrationtools/migrate_group.pl /etc/group > /gen/group6.ldif
 # client6-run-postfix ends here
-# Настройка\ аутентификации\ пользователей\ при\ помощи\ LDAP:1 ends here
+# Используемые\ пакеты:1 ends here

15154628e8001723f4d057083d979fc39780176c
Evgeny Boykov <artscan@list.ru>
2016-10-25 14:19:50 +1000

    add <<add-user>>

--- schema/build.sh
+++ schema/build.sh
@@ -5,7 +5,9 @@ cd /schema
 
 ldapmodify -v -D cn=Manager,cn=config -f domain.ldif -x -w $LDAP_ROOT_PASSWORD
 ldapadd -x -D 'cn=Manager,dc=mercury,dc=febras,dc=net' -w $LDAP_MANAGER_PASSWORD -f base.ldif
+# build-schema ends here
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#new-user][add-user]]
 ldapadd -x -D 'cn=Manager,dc=mercury,dc=febras,dc=net' -w $LDAP_MANAGER_PASSWORD -f user.ldif
 ldapsearch -x -LLL -D 'cn=Manager,cn=config' -b 'dc=mercury,dc=febras,dc=net' '*' -w $LDAP_ROOT_PASSWORD
-# build-schema ends here
+# add-user ends here
 # Подготовка\ к\ тестированию\ аутентификации\ при\ помощи\ LDAP:1 ends here

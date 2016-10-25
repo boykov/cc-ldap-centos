c3d97b0898b0165d0f4c626f15bf230b7ce7349e
Evgeny Boykov <artscan@list.ru>
2016-10-25 11:38:25 +1000

    add <<build-schema>>, fix paths

--- schema/build.sh
+++ schema/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#minimal-schema][build-schema]]
 
 cd /schema
 
@@ -6,3 +7,5 @@ ldapmodify -v -D cn=Manager,cn=config -f domain.ldif -x -w $LDAP_ROOT_PASSWORD
 ldapadd -x -D 'cn=Manager,dc=mercury,dc=febras,dc=net' -w $LDAP_MANAGER_PASSWORD -f base.ldif
 ldapadd -x -D 'cn=Manager,dc=mercury,dc=febras,dc=net' -w $LDAP_MANAGER_PASSWORD -f user.ldif
 ldapsearch -x -LLL -D 'cn=Manager,cn=config' -b 'dc=mercury,dc=febras,dc=net' '*' -w $LDAP_ROOT_PASSWORD
+# build-schema ends here
+# Подготовка\ к\ тестированию\ аутентификации\ при\ помощи\ LDAP:1 ends here

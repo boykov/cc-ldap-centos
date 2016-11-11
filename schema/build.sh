515c1bcc25ce99f96aa48eb99790ea752e512055
Evgeny Boykov <artscan@list.ru>
2016-11-11 12:18:30 +1000

    add sudo.ldif

--- schema/build.sh
+++ schema/build.sh
@@ -5,6 +5,7 @@ cd /schema
 
 ldapmodify -v -D cn=Manager,cn=config -f domain.ldif -x -w $LDAP_ROOT_PASSWORD
 ldapadd -x -D 'cn=Manager,dc=mercury,dc=febras,dc=net' -w $LDAP_MANAGER_PASSWORD -f base.ldif
+ldapadd -x -D 'cn=Manager,dc=mercury,dc=febras,dc=net' -w $LDAP_MANAGER_PASSWORD -f sudo.ldif
 # build-schema ends here
 # [[file:~/git/cc/cc-ldap-centos/docs/index.org::#new-user-ldap][add-user]]
 ldapadd -x -D 'cn=Manager,dc=mercury,dc=febras,dc=net' -w $LDAP_MANAGER_PASSWORD -f user.ldif

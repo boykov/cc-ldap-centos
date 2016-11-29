ad3813decf39899ea9247b6ee5f5d6b56e934c71
Evgeny Boykov <artscan@list.ru>
2016-11-29 16:00:51 +1000

    ref.: improve unit tests (mutual independence)

--- schema/schema.sh
+++ schema/schema.sh
@@ -3,6 +3,7 @@
 
 function password() {
     ldappasswd -h $1 -x -D "uid=username,ou=users,dc=mercury,dc=febras,dc=net" -w p@ssw0rd -s new_p@ssw0rd
+    ldappasswd -h $1 -x -D "uid=username,ou=users,dc=mercury,dc=febras,dc=net" -w new_p@ssw0rd -s p@ssw0rd
 }
 
 function password_out() {
@@ -10,39 +11,31 @@ cat <<EOF
 EOF
 }
 
-function modify() {
 # schemash-prefix ends here
 # [[file:~/git/cc/cc-ldap-centos/docs/index.org::#schema.sh][modify-sh]]
-    ldapmodify -h $1 -x -D "uid=username,ou=users,dc=mercury,dc=febras,dc=net" -w new_p@ssw0rd <<EOF
+function modify() {
+    ldapmodify -h $1 -x -D "uid=username,ou=users,dc=mercury,dc=febras,dc=net" -w p@ssw0rd <<EOF
 dn: uid=username,ou=users,dc=mercury,dc=febras,dc=net
 changetype: modify
 replace: loginShell
 loginShell: /bin/sh
 -
 EOF
-# modify-sh ends here
-# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#schema.sh][schemash-postfix]]
+    ldapsearch -x -h $1 -LLL -D 'cn=Manager,dc=mercury,dc=febras,dc=net' -b 'dc=mercury,dc=febras,dc=net' '(loginShell=*)' -w manager | grep loginShell
 }
 
 function modify_out() {
 cat <<EOF
 modifying entry "uid=username,ou=users,dc=mercury,dc=febras,dc=net"
 
-EOF
-}
-
-function login() {
-    ldapsearch -x -h $1 -LLL -D 'cn=Manager,dc=mercury,dc=febras,dc=net' -b 'dc=mercury,dc=febras,dc=net' '(loginShell=*)' -w manager | grep loginShell
-}
-
-function login_out() {
-cat <<EOF
 loginShell: /bin/sh
 EOF
 }
+# modify-sh ends here
+# [[file:~/git/cc/cc-ldap-centos/docs/index.org::#schema.sh][schemash-postfix]]
 
 function ssh() {
-    sshpass -p new_p@ssw0rd ssh -o "GSSAPIAuthentication no" -o "UserKnownHostsFile /dev/null" -o StrictHostKeyChecking=no -o "VerifyHostKeyDNS no" -t username@$2 sudo ls /root/Dockerfile5 2> /dev/null || true
+    sshpass -p p@ssw0rd ssh -o "GSSAPIAuthentication no" -o "UserKnownHostsFile /dev/null" -o StrictHostKeyChecking=no -o "VerifyHostKeyDNS no" -t username@$2 sudo ls /root/Dockerfile5 2> /dev/null || true
 }
 
 function ssh_out() {
@@ -97,7 +90,7 @@ EOF
 }
 
 function getent() {
-    sshpass -p new_p@ssw0rd ssh -o "GSSAPIAuthentication no" -o "UserKnownHostsFile /dev/null" -o StrictHostKeyChecking=no -o "VerifyHostKeyDNS no" -t username@$2 sudo getent passwd username 2> /dev/null || true
+    sshpass -p p@ssw0rd ssh -o "GSSAPIAuthentication no" -o "UserKnownHostsFile /dev/null" -o StrictHostKeyChecking=no -o "VerifyHostKeyDNS no" -t username@$2 sudo getent passwd username 2> /dev/null || true
 }
 
 function getent_out() {
@@ -107,7 +100,7 @@ EOF
 }
 
 function homedir() {
-    sshpass -p new_p@ssw0rd ssh -o "GSSAPIAuthentication no" -o "UserKnownHostsFile /dev/null" -o StrictHostKeyChecking=no -o "VerifyHostKeyDNS no" -t username@$2 ls /home 2> /dev/null || true
+    sshpass -p p@ssw0rd ssh -o "GSSAPIAuthentication no" -o "UserKnownHostsFile /dev/null" -o StrictHostKeyChecking=no -o "VerifyHostKeyDNS no" -t username@$2 ls /home 2> /dev/null || true
 }
 
 function homedir_out() {

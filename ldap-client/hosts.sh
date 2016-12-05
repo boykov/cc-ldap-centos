bf2e23e4696c6e10ecb42e3e04fc0e568321fcd8
Evgeny Boykov <artscan@list.ru>
2016-12-05 14:19:38 +1000

    add smpwd for authenticator usage with sendmail

--- ldap-client/hosts.sh
+++ ldap-client/hosts.sh
@@ -9,6 +9,7 @@ echo "$line $line2.localdomain $ln2" >> /etc/hosts
 
 echo pwcheck_method:pam > /etc/sasl2/Sendmail.conf
 
+cp /root/smpwd /etc/mail/ldap-secret
 cp /etc/mail/sendmail.mc /etc/mail/sendmail.mc.bak
 cp -f /root/sendmail.mc /etc/mail/sendmail.mc
 m4 /root/sendmail.mc > /etc/mail/sendmail.cf

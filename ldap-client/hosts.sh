cc9961e33e6fe4acaaf2178d5359618915004b47
Evgeny Boykov <artscan@list.ru>
2016-12-05 15:54:14 +1000

    fix /etc/hosts: rm 172.17.0.* ip (WARNING!)

--- ldap-client/hosts.sh
+++ ldap-client/hosts.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 line=$(head -n 1 /etc/hosts)
+ip=$(echo $line | awk '{print $1}')
 line2=$(echo $line | awk '{print $2}')
 ln=$(tail -n 1 /etc/hosts)
 ln2=$(echo $ln | awk '{print $2}')
-     
-echo "$line $line2.localdomain $ln2" >> /etc/hosts
+
+cp /etc/hosts /etc/hosts.tmp
+sed -i '$d' /etc/hosts.tmp
+cp /etc/hosts.tmp /etc/hosts
+
+echo "$ip $ln2.localdomain $line2.localdomain $ln2 $line2" >> /etc/hosts
 
 echo pwcheck_method:pam > /etc/sasl2/Sendmail.conf
 

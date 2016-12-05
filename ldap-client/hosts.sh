ddb2fc2d65deaaab9ddd13964fbf24bda6887408
Evgeny Boykov <artscan@list.ru>
2016-12-05 14:17:35 +1000

    fix: wrong extensions

--- ldap-client/hosts.sh
+++ ldap-client/hosts.sh
@@ -9,6 +9,6 @@ echo "$line $line2.localdomain $ln2" >> /etc/hosts
 
 echo pwcheck_method:pam > /etc/sasl2/Sendmail.conf
 
-cp /etc/mail/sendmail.cf /etc/mail/sendmail.cf.bak
-cp -f /root/sendmail.mc /etc/mail/sendmail.cf
+cp /etc/mail/sendmail.mc /etc/mail/sendmail.mc.bak
+cp -f /root/sendmail.mc /etc/mail/sendmail.mc
 m4 /root/sendmail.mc > /etc/mail/sendmail.cf
